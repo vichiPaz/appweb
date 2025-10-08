ae574b3c2e199fece5ab0698d9e278811a2b9867
Vichi Troncoso Molina <vtroncosomolina@gmail.com>
2025-10-08 18:52:14 -0300

    se carga la base del proyecto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <h1>App</h1>
+  <nav>
+    <router-link to="/">Home |</router-link>
+    <router-link to="/login">Login |</router-link>
+    <router-link to="/register">Register </router-link>
+    <button>Logout</button>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped></style>
